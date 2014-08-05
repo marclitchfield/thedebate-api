2efb78e352a16d8515640334aaacb75ee9e94a68
Marc Litchfield <marclitchfield@gmail.com>
2014-08-05 02:53:16 +0000

    Server api folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/api', function(req, res){
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.end("Hello World\n");
 });
