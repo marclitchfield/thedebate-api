ff54884ae13a8db537cd35b3fa288d83fe617542
marclitchfield <marclitchfield@gmail.com>
2014-08-04 19:32:03 -0700

    Hello world

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("Hello World\n");
+});
+
+var server = app.listen(9003, function() {
+  console.log('Listening on port %d', server.address().port);
+});
